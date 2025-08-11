981b30fc5763062caf690f76b5fa5fa351fbcc20
SevenWoos <swenwu96@gmail.com>
2025-08-11 14:10:48 -0700

    added imports

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 from flask import Flask, render_template, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
